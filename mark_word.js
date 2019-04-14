172b1186b94565ca5338b492bb1c64e8e683b00e
Shakbia Moshiri <hi@shakiba.it>
2019-04-14 11:36:26 +0430

    Remove comments

--- mark_word.js
+++ mark_word.js
@@ -10,10 +10,9 @@ const listname = process.argv[3];
 
 const file = fs.readFileSync( filename );
 const list = fs.readFileSync( listname );
-// const list = [ "i", "and", "your", "with", "the", "soon", "ll", "it", "is", "you" ];
+
 const lines = file.toString().split( "\n" );
 const vocabularies = list.toString().split( /\W+/g ).filter(( line ) => line !== "" );
-// const list = [ "a", "d", "e", "m", "t" ];
 
 lines.forEach( ( line ) => {
     const words = line.split( /\W+/g );
