76209fe92c7d5f44186e070df4ea9f10dbf64c52
Shakiba Moshiri <hi@shakiba.it>
2020-06-14 10:43:57 +0430

    Refactor, to get more accurate result

--- oxforddictionaries.com/oxforddictionaries.com.download.sh
+++ oxforddictionaries.com/oxforddictionaries.com.download.sh
@@ -5,10 +5,12 @@ url=http://www.oxfordlearnersdictionaries.com/definition/english/
 for word in $@; do
     if ! [ -e $word.mp3 ]; then
         echo -e "There is no  \033[1;32m$word.mp3\033[0m so looking for it ..." 
-            lynx -dump -source $url$word\_1?q=$word | \
-            perl -lne '/http[^"]+\.mp3/ && print $&' | \
-            head -n 1 | \
-            xargs -I xxx wget -q -nc --show-progress -c xxx -O $word.mp3
+        lynx -dump -source $url$word\_1?q=$word | \
+        egrep -o 'http[^ ]+?mp3' | \
+        sort | \
+        uniq | \
+        grep $word | \
+        xargs -I xxx wget -q -nc --show-progress -c xxx
     else
         echo -e "You have the file: \033[1;32m$word.mp3\033[0m" 
     fi
